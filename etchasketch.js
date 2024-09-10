9d1f657f019be19fbbe8cf77c20c795636877efd
Hayden Dingman <haydendingman@gmail.com>
2024-09-09 23:23:08 -0700

    Add event handler for squares turning black in Etch-A-Sketch

--- etchasketch.js
+++ etchasketch.js
@@ -1,9 +1,15 @@
 const etchasketch = document.querySelector(".container");
 
-for (let counter = 0; counter < (16 * 16); counter++) {
+for (let counter = 0; counter < (256); counter++) {
     console.log("for loop");
     let div = document.createElement("div");
+    div.addEventListener("mouseover", darken);
     console.log("div created: " + counter);
     div.setAttribute("style", "background-color:lightgrey; box-sizing:border-box; border:solid black 1px; flex:1 0 6.25%;")
     etchasketch.appendChild(div);
 };
+
+function darken(event) {
+    this.style.backgroundColor = "black";
+    event.stopPropagation();
+}
