0a521a6e996d43b4115b468d6c397f264ad66724
Hayden Dingman <haydendingman@gmail.com>
2024-09-09 22:53:23 -0700

    Use border-box to split grid into individual squares visually.

--- etchasketch.js
+++ etchasketch.js
@@ -4,10 +4,6 @@ for (let counter = 0; counter < (16 * 16); counter++) {
     console.log("for loop");
     let div = document.createElement("div");
     console.log("div created: " + counter);
-    if (counter % 2 === 0) {
-        div.setAttribute("style", "background-color:white; flex:1 0 6.25%;")
-    } else {
-        div.setAttribute("style", "background-color:black; flex:1 0 6.25%;")
-    }
+    div.setAttribute("style", "background-color:lightgrey; box-sizing:border-box; border:solid black 1px; flex:1 0 6.25%;")
     etchasketch.appendChild(div);
 };
